89daa5953a0485f2066f77d9743ce5a5a8a3f871
TonyBao0403 <tony@megalive.com.tw>
2018-10-30 10:08:12 +0800

    App.blade Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,6 +46,7 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            
                             <li><a href="/user">User 管理</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
